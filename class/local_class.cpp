1a0d59e00d3832daf772903a70a65959bd76d6fb
ABHISHEK FADAKE <31249309+fadkeabhi@users.noreply.github.com>
2022-12-07 20:17:59 +0530

    Add files via upload

---
+++ class/local_class.cpp
@@ -0,0 +1,57 @@
+// C++ program without any compilation error to demonstrate 
+// that all the methods of Local classes must be defined 
+// inside the class only 
+#include <iostream> 
+
+using namespace std; 
+
+  
+
+void fun() 
+{ 
+
+    class Test // local to fun 
+
+    { 
+
+    public: 
+    
+   // static int a;
+   // above commented code will give error because A Local class cannot contain static data members
+
+        // Fine as the method is defined 
+
+        // inside the local class 
+
+        void method() 
+
+        { 
+        
+        
+        
+            cout << "Local Class method() called"; 
+        } 
+        
+        void notallowed();
+        
+
+    }; 
+//    void Test::notallowed() { cout << "Local Class notallowed()"; } 
+    //Declaring A Function And Then IsIs Not Allowed In Local Class
+
+  
+
+    Test t; 
+
+    t.method(); 
+} 
+
+  
+
+int main() 
+{ 
+
+    fun(); 
+
+    return 0; 
+}

87cfe66c573bc469775d9867463de80e497df64f
ABHISHEK FADAKE <31249309+fadkeabhi@users.noreply.github.com>
2022-12-08 12:01:32 +0530

    Create constant_keyword.cpp

---
+++ unit_1/constant_keyword.cpp
@@ -0,0 +1,23 @@
+// C++ program to demonstrate the
+// the above concept
+#include <iostream>
+
+using namespace std;
+ 
+// Driver Code
+
+int main()
+{
+ 
+
+    // const int x;  CTE error
+
+    // x = 9;   CTE error
+
+    const int y = 10;
+
+    cout << y;
+ 
+
+    return 0;
+}
